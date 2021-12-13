7fe392c0b79c7af672b0c027dff562f431329e86
Seb <sebcyde@hotmail.com>
2021-12-13 23:25:29 +0000

    added in floating gray buttons

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import NavbarComponent from '../Navbar/NavbarComponent';
+import { Icon, Button } from 'react-materialize';
 import './HomePage.css';
 
 function HomePage() {
 	return (
 		<div className="HomePageContainer">
 			<NavbarComponent />
+
+			<div className="FloatingGrayButtons">
+				<Button
+					className="GrayButton"
+					fab={{
+						direction: 'left',
+					}}
+					floating
+					large
+					node="button"
+				>
+					<Button
+						className="GrayButton"
+						floating
+						icon={<Icon>insert_chart</Icon>}
+						node="button"
+					/>
+					<Button
+						className="GrayButton"
+						floating
+						icon={<Icon>format_quote</Icon>}
+						node="button"
+					/>
+					<Button
+						className="GrayButton"
+						floating
+						icon={<Icon>publish</Icon>}
+						node="button"
+					/>
+					<Button
+						className="GrayButton"
+						floating
+						icon={<Icon>attach_file</Icon>}
+						node="button"
+					/>
+				</Button>
+			</div>
 		</div>
 	);
 }
