b67d186f0021c866152f9c79b805f5289a378ba1
Seb <sebcyde@hotmail.com>
2021-12-23 14:29:17 +0000

    removed materialize nav and began making vertical manual navbar to be used with AOS

--- src/Components/Navbar/NavbarComponent.js
+++ src/Components/Navbar/NavbarComponent.js
@@ -1,44 +1,17 @@
 import React from 'react';
-import { Navbar, NavItem, Icon, Switch } from 'react-materialize';
 import './NavbarComponent.css';
 
 function NavbarComponent() {
 	return (
 		<div className="NavBarContainer">
-			<Navbar
-				className="NavBar"
-				alignLinks="left"
-				brand={
-					<a className="brand-logo right" href="#">
-						Socialiser
-					</a>
-				}
-				id="mobile-nav"
-				menuIcon={<Icon>menu</Icon>}
-				options={{
-					draggable: true,
-					edge: 'left',
-					inDuration: 250,
-					onCloseEnd: null,
-					onCloseStart: null,
-					onOpenEnd: null,
-					onOpenStart: null,
-					outDuration: 200,
-					preventScrolling: true,
-				}}
-			>
-				<NavItem onClick={function noRefCheck() {}}>Home</NavItem>
-				<NavItem href="components.html">Friends</NavItem>
-				<NavItem href="components.html">My Profile</NavItem>
-
-				<Switch
-					className="LightSwitch"
-					id="Switch-20"
-					offLabel={<i class="material-icons">wb_sunny</i>}
-					onChange={function noRefCheck() {}}
-					onLabel={<i class="material-icons">brightness_3</i>}
-				/>
-			</Navbar>
+			<ul className="Navbar">
+				<li>Home</li>
+				<li>Test2</li>
+				<li>Test3</li>
+				<li>Test4</li>
+				<li>Test5</li>
+				<li>Test6</li>
+			</ul>
 		</div>
 	);
 }
